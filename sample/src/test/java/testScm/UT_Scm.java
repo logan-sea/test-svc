6de9ae98c00127bff2de0677468483f57911e208
Anand Loganathan <logan@uplevelteam.com>
2021-04-07 12:45:19 -0700

    Adding the sample-svc submodule and testing that using an Unit Test.

--- sample/src/test/java/testScm/UT_Scm.java
+++ sample/src/test/java/testScm/UT_Scm.java
@@ -1,20 +1,21 @@
 package testScm;
 
-import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
-import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class UT_Scm {
+    private String pathToQuery = "../sampleSvc/mars-rover/" +
+            "src/test/resources/integration/exporter/gitlab/";
 
     @Test
     public void testScm() throws IOException {
-        String codeJiraQuery = "integration/exporter/gitlab/Repos.sql";
-        String query = (IOUtils.toString(
-                getClass().getClassLoader().getResourceAsStream(codeJiraQuery)
-                , Charset.defaultCharset()));
+        String codeJiraQuery = pathToQuery + "gitlab_code_jira_by_url.sql";
+
+        String query = new Scanner(new File(codeJiraQuery)).useDelimiter("\\Z").next();
         Assert.assertTrue(query.length() > 0);
     }
 }
